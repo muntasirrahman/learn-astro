53629748f4b5fc2fbad738930d35391edc641f78
najwadv <najwaadiva.0302@gmail.com>
2023-11-09 20:40:37 +0700

    Token Frontend Codes

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -1,3 +1,82 @@
-export const Token = () => {
-	return <h1>Token form here</h1>;
+import { useState } from "react";
+import type { Candidate } from "..";
+
+export const TokenUser = () => {
+  const [token, setToken] = useState("");
+  const [candidate, setCandidate] = useState<Candidate>({} as Candidate);
+  const [received, setReceived] = useState(false);
+
+  const handleToken = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const tokenBaru = event.target.value;
+    setToken(tokenBaru);
+  };
+
+  const handleButtonClick = () => {
+    const Token: string = "";
+    console.log("Token: " + JSON.stringify(Token));
+
+    fetch("http://localhost:4321/api/students", {
+      method: "POST",
+    })
+      .then((response) => {
+        if (response.ok) {
+            setReceived(true);
+        } else {
+          alert("Token yang anda masukkan salah.");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Gagal, karena: " + error.message);
+      });
+  };
+
+  const handleButtonCandidateClick = () => {
+    setReceived(false);
+  }
+
+  return (
+    <div className="container mx-auto">
+      {received === false && (
+        <div id="TokenForm">
+          <div>Token</div>
+          <div className="mt-4">
+            <input
+              className="p-2"
+              type="text"
+              placeholder="Ketikkan token disini"
+              value={token}
+              onChange={handleToken}
+            />
+          </div>
+          <div className="mt-4">
+            <button
+              onClick={handleButtonClick}
+              className="inline-block px-5 py-3
+                        bg-blue-400 text-white rounded-lg shadow-lg
+                        tracking-wider text-sm"
+            >
+              Enter
+            </button>
+          </div>
+        </div>
+      )}
+      {received && (
+        <div id="Candidate">
+          <div className="p-2">Nama: Shu Yamino</div>
+          <div className="p-2">Jabatan: Secretary</div>
+          <div className="p-2">Tanggal Lahir: April 4, 2000</div>
+          <div className="mt-4">
+            <button
+            onClick={handleButtonCandidateClick}
+              className="inline-block px-5 py-3
+                    bg-blue-400 text-white rounded-lg shadow-lg
+                    tracking-wider text-sm"
+            >Mulai Tes
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,12 @@
 export type Peserta = {
-	nama: string;
-	iq: number;
-	sekolah: string;
-	lulus?: boolean;
-};
+    nama: string;
+    iq: number;
+    sekolah: string;
+    lulus?: boolean;
+}
+
+export type Candidate = {
+    name: string;
+    jobTitle: string;
+    birthDate: Date;
+}
