b70057983f47695ab9734a81bc63d25a3a79e6f2
muntasirrahman <muntasir.rahman@gmail.com>
2023-11-10 17:57:28 +0700

    Tailwind CSS Grid tutorial

---
+++ src/components/Grid.tsx
@@ -0,0 +1,13 @@
+export const GridComponent = () => {
+	return (
+		<div className="grid grid-cols-2 border-2 border-red-200">
+			<div>01</div>
+			<div>01</div>
+			<div className="bg-green-400">01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+		</div>
+	);
+};
